f366529e4baea73a824c3f979a195bd7606923ae
Cody Lillywhite <codylillyw@gmail.com>
2020-10-17 13:34:49 -0400

    week5

--- main.js
+++ main.js
@@ -25,6 +25,30 @@ function displayLinks() {
     {
       label: "Week5 notes",
       url: "week5/index.html"
+    },
+    {
+      label: "Week6 notes",
+      url: "week6/index.html"
+    },
+    {
+      label: "Week7 notes",
+      url: "week7/index.html"
+    },
+    {
+      label: "Week8 notes",
+      url: "week8/index.html"
+    },
+    {
+      label: "Week9 notes",
+      url: "week9/index.html"
+    },
+    {
+      label: "Week10 notes",
+      url: "week10/index.html"
+    },
+    {
+      label: "Week11 notes",
+      url: "week11/index.html"
     }
   ];
   const fullList = document.createElement('ol');
